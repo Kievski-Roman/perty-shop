6cd391475e7ad788a23c9e855a0ffc3603d7d442
Kievski-Roman <rkievski@gmail.com>
2024-01-09 03:27:27 +0100

    Первый коммит

---
+++ blocks/footer/footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+    <div class="footer__top">
+    <div class="footer__info">
+        <h4 class="footer__title-info">
+            INFO
+        </h4>
+        <a href="#" class="footer__link">FAQ</a>
+        <a href="#" class="footer__link">Shipping & Returns</a>
+        <a href="#" class="footer__link">Store Policy</a>
+        <a href="#" class="footer__link">Payment Methods</a>
+    </div>
+    <div class="footer__info">
+        <h4 class="footer__title-info">
+            CUSTOMER SERVICE
+        </h4>
+        <p class = "footer__phone-namber">1-800-000-0000</p>
+        <a href="#" class="footer__link">info@my-domain.com</a>
+    </div>
+    <div class="footer__info">
+        <h4 class="footer__title-info">
+        FOLLOW OUR PAWPRINTS
+        </h4>
+        <div class="footer__social-wrap">
+        <a href="#" class="footer__social"><img src="../../img/instagram.svg" alt=""></a>
+        <a href="#" class="footer__social"><img src="../../img/telegram.svg" alt=""></a>
+        <a href="#" class="footer__social"><img src="../../img/facebook.svg" alt=""></a>
+        </div>
+        
+    </div>
+    </div>
+</footer>
